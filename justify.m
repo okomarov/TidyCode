b655223274a5d684db66157f86e4097087b41abe
oleg komarov <o.komarov11@imperial.ac.uk>
2014-08-15 20:17:22 +0100

    [Minor] wrapped into function (no in/out), justified :)
    Signed-off-by: oleg komarov <o.komarov11@imperial.ac.uk>

--- justify.m
+++ justify.m
@@ -1,18 +1,19 @@
+function justify
 % Get active document
-obj = matlab.desktop.editor.getActive;
+obj   = matlab.desktop.editor.getActive;
 text  = obj.Text;
 lines = matlab.desktop.editor.textToLines(text);
 
 % Sandwich lines between two empty ones
-lines = [{''}; lines; {''}];
+lines  = [{''}; lines; {''}];
 nlines = numel(lines);
 
 % Parse file into a tree
 tree = mtree(text);
 
-% Get keywords (ELSEIF separately)
+% Kerywords idx (ELSEIF separately)
 lineno.Keywords      = false(nlines,1);
-keywords             = {'IF','ELSE','TRY','CATCH','WHILE','FOR','PARFOR','FUNCTION','CASE','OTHERWISE'}; 
+keywords             = {'IF','ELSE','TRY','CATCH','WHILE','FOR','PARFOR','FUNCTION','CASE','OTHERWISE'};
 tmp                  = tree.mtfind('Kind',keywords);
 pos                  = [tmp.lineno; tmp.lastone];
 tmp                  = tree.mtfind('Kind','ELSEIF');
@@ -25,43 +26,43 @@ lineno.Empty = cellfun('isempty',regexp(lines, '[^ \n]','once'));
 % Comment line idx
 lineno.Comment = ~cellfun('isempty',regexp(lines, '^ *%[^%]','once'));
 
-% Double %% 
+% Double %%
 lineno.Cell = ~cellfun('isempty',regexp(lines, '^ *%%','once'));
 
 % Char index
-chidx = repmat('c',nlines,1);
+chidx                  = repmat('c',nlines,1);
 chidx(lineno.Keywords) = ' ';
-chidx(lineno.Empty) = ' ';
-chidx(lineno.Cell) = ' ';
-chidx(lineno.Comment) = '%';
+chidx(lineno.Empty)    = ' ';
+chidx(lineno.Cell)     = ' ';
+chidx(lineno.Comment)  = '%';
 
 % Create blocks
-ii = 1;
+ii     = 1;
 fromto = zeros(2, ceil(nlines/2));
-b = 1;
+b      = 1;
 cblock = true;
 while ii < nlines
     ii = ii + 1;
     if cblock
         if chidx(ii) == 'c'
             fromto(1,b) = ii;
-            cblock = false;
+            cblock      = false;
         end
     elseif chidx(ii) == ' '
         fromto(2,b) = ii;
-        b = b + 1;
-        cblock = true;
+        b           = b + 1;
+        cblock      = true;
     end
 end
 fromto = fromto(:,fromto(1,:)~=0 & diff(fromto) > 1);
 
 % Get line numbers of assignments
-lineno.Assign = false(nlines,1);
-asgpos = tree.asgvars.lineno + 1;
+lineno.Assign         = false(nlines,1);
+asgpos                = tree.asgvars.lineno + 1;
 lineno.Assign(asgpos) = true;
 
 % Bring all assignment to 'LHS = RHS'
-idx = lineno.Assign & chidx == 'c';
+idx        = lineno.Assign & chidx == 'c';
 lines(idx) = regexprep(lines(idx),' *= *',' = ','once');
 
 % LOOP by block and justify
@@ -69,7 +70,7 @@ lines(idx) = regexprep(lines(idx),' *= *',' = ','once');
 for ii = 1:2:size(fromto,2)*2
     if counts(ii) > 0
         linepos = asgpos(subs == ii);
-        tmp = lines(linepos);
+        tmp     = lines(linepos);
         if counts(ii) > 1
             pos    = regexp(tmp,'=','once');
             maxp   = max([pos{:}])-1;
@@ -80,3 +81,4 @@ for ii = 1:2:size(fromto,2)*2
     end
 end
 obj.Text = matlab.desktop.editor.linesToText(lines);
+end
