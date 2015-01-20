30f0c7cb23d227bc2ece681f0041de1422f72dc5
Oleg Komarov <o.komarov11@imperial.ac.uk>
2015-01-20 18:45:48 +0000

    Programmatic add shortcut to QAB

--- install_justify.m
+++ install_justify.m
@@ -1,49 +1,34 @@
 function install_justify
 % INSTALL_JUSTIFY Adds settings to the startup file and the folder to the MATLAB path
 
-% Check if startup already exists
-if exist('startup','file') == 2
-    isnew = false;
-    fullpath = which('startup.m');
-else
-    isnew = true;
-    fullpath = fullfile(regexprep(userpath,';$',''), 'startup.m');
-end
-
-% Open to append
-fid = fopen(fullpath, 'a+');
-fseek(fid,0,'bof');
-txt = textscan(fid, '%s','Delimiter','','EndOfLine','');
-txt = txt{:};
-fseek(fid,0,'eof');
-
-% Check if already installed
-if ~isnew && any(~cellfun('isempty', regexp(txt,'^%% justify_settings')))
-    warning('install_justify:isinstalled','JUSTIFY appears to be already installed.')
-    return
-end
-
-justifycommands = [...
-    '%% justify_settings (this title is used as anchor, do NOT edit)\n'
-];    
-
-% Add justify startup commands to the startup file
-txt = [txt; justifycommands];
-fprintf(fid, '%s', txt);
-fclose(fid);
-
 % Add path
 folderpath = fileparts(mfilename('fullpath'));
 addpath(folderpath)
 savepath
 
 % Add shortcut
-s = struct('label'   , 'justify',...
+s  = struct('label'   , 'justify',...
            'callback', 'justify',...
            'icon'    , fullfile(folderpath,'justify.jpg'),...
            'category', 'Shortcuts',...
            'editable', 'true');
+su = com.mathworks.mlwidgets.shortcuts.ShortcutUtils();
+su.addShortcutToBottom(s.label, s.callback, s.icon, s.category, s.editable);
 
-com.mathworks.mlwidgets.shortcuts.ShortcutUtils.addShortcutToBottom(s.label, s.callback, s.icon, s.category, s.editable);
+% Add shortcut to Quick Access Bar
+% TODO: add only if does not exist
+desktop  = com.mathworks.mde.desk.MLDesktop.getInstance();
+QAB      = desktop.getQuickAccessConfiguration;
+import com.mathworks.toolstrip.factory.*
+toolPath = TSToolPath(TSToolPath('shortcuts','general'),'justify','matlab_shortcut_toolset');
+QAB.addTool(toolPath)
+QAB.setLabelVisible(toolPath,false)
+
+% Set button mnemonic
+% TODO: get component by full to0lname
+jDesktop = desktop.getMainFrame();
+jQAB     = jDesktop.getQuickAccessBar().getComponent();
+jButton  = jQAB.getComponent(jQAB.getComponentCount-2);
+jButton.setButtonMnemonic('10');
 
 end
